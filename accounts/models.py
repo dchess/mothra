06879fdcecf29194889e037bd081f9feeb2ebf0b
dhess <dan.hess@kippbayarea.org>
2019-10-23 20:51:22 -0700

    Add grade model and migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Grade(models.Model):
+    level = models.IntegerField(
+        validators=[MaxValueValidator(12), MinValueValidator(-1)]
+    )
+    name = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("level",)
