bb23ab1bcff1dd9c567fde08bf234f71bd6ea2e2
dhess <dan.hess@kippbayarea.org>
2019-11-05 09:02:15 -0800

    Add domain model tests

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import pytest
+from inventory.models import Domain
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def domain():
+    return Domain.objects.create(name="Test Domain")
+
+
+def test_domain_string_representation(domain):
+    assert str(domain) == domain.name
+
+
+def test_domain_name_max_length(domain):
+    with pytest.raises(ValidationError):
+        domain.name = "x" * 51
+        domain.full_clean()
