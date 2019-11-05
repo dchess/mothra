4e2d058d875396f11d4b4c126712ec60a261b6c5
dhess <dan.hess@kippbayarea.org>
2019-11-05 09:05:19 -0800

    Add subject model tests

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from inventory.models import Domain
+from inventory.models import Domain, Subject
 from django.core.exceptions import ValidationError
 
 pytestmark = pytest.mark.django_db
@@ -18,3 +18,18 @@ def test_domain_name_max_length(domain):
     with pytest.raises(ValidationError):
         domain.name = "x" * 51
         domain.full_clean()
+
+
+@pytest.fixture
+def subject():
+    return Subject.objects.create(name="Test Subject")
+
+
+def test_subject_string_representation(subject):
+    assert str(subject) == subject.name
+
+
+def test_subject_name_max_length(subject):
+    with pytest.raises(ValidationError):
+        subject.name = "x" * 26
+        subject.full_clean()
