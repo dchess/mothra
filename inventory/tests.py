64135aa46299f7df7e6c7f9ada11be6086c1b85b
dhess <dan.hess@kippbayarea.org>
2019-11-05 09:16:27 -0800

    Add product tests

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,6 +1,8 @@
 import pytest
-from inventory.models import Domain, Subject
+from inventory.models import Domain, Subject, Product
 from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+from django.db.models.deletion import ProtectedError
 
 pytestmark = pytest.mark.django_db
 
@@ -33,3 +35,40 @@ def test_subject_name_max_length(subject):
     with pytest.raises(ValidationError):
         subject.name = "x" * 26
         subject.full_clean()
+
+
+@pytest.fixture
+def product(domain, subject):
+    return Product.objects.create(name="Test Product", domain=domain, subject=subject)
+
+
+def test_product_string_representation(product):
+    assert str(product) == product.name
+
+
+def test_product_name_max_length(product):
+    with pytest.raises(ValidationError):
+        product.name = "x" * 101
+        product.full_clean()
+
+
+def test_product_requires_domain(product, subject):
+    with pytest.raises(IntegrityError):
+        product = Product.objects.create(name="Test Product 2", subject=subject)
+        product.full_clean()
+
+
+def test_product_requires_subject(product, domain):
+    with pytest.raises(IntegrityError):
+        product = Product.objects.create(name="Test Product 3", domain=domain)
+        product.full_clean()
+
+
+def test_product_protects_deleting_domain(product, domain):
+    with pytest.raises(ProtectedError):
+        domain.delete()
+
+
+def test_product_protects_deleting_subject(product, subject):
+    with pytest.raises(ProtectedError):
+        subject.delete()
