25c315e8d2555602adca4ef23e6eaf8b00bf2f72
dhess <dan.hess@kippbayarea.org>
2019-11-05 09:16:20 -0800

    Add product model and migration

--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 
 class Domain(models.Model):
@@ -19,3 +20,21 @@ class Subject(models.Model):
 
     class Meta:
         ordering = ("name",)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    domain = models.ForeignKey(Domain, on_delete=models.PROTECT)
+    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("name",)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ("domain", "subject")
+    list_display = ("name", "domain", "subject")
+    search_fields = ["name"]
