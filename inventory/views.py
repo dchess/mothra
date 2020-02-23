2c07942ec0b49520d316ab6b03dee7e3457d8b44
dhess <dan.hess@kippbayarea.org>
2020-02-22 19:13:44 -0800

    Fix divide by zero error

--- inventory/views.py
+++ inventory/views.py
@@ -49,9 +49,10 @@ class ProductDetail(LoginRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         reviews = Usage.objects.filter(product=self.get_object())
         total_reviews = float(reviews.count())
-        promoters = (reviews.filter(rating=5).count() / total_reviews) * 100
-        detractors = (reviews.filter(rating__lte=3).count() / total_reviews) * 100
-        context["NPS"] = int(promoters - detractors)
+        if total_reviews > 0:
+            promoters = (reviews.filter(rating=5).count() / total_reviews) * 100
+            detractors = (reviews.filter(rating__lte=3).count() / total_reviews) * 100
+            context["NPS"] = int(promoters - detractors)
         return context
 
 
